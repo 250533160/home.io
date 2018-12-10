83f014c0caf9ee055a349020060e548d05fd995e
chenz15 <chenz15@miamioh.edu>
2018-12-09 23:30:36 -0500

    import js link from local

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from './components/App';
 // import bootstrap css
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+// import js
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/umd/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(
   // <BrowserRouter>
