4579e88e3ac330d5068e1d1479b27d1345e75f60
chenz15 <chenz15@miamioh.edu>
2018-12-09 23:27:57 -0500

    Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    constructor(props){
+        super(props);
+        this.state ={};
+
+    }
+
+    render(){
+        return (
+            <div className="footer text-center">
+                <p>Copyright © by Zongxin Chen. All Rights Reserved.</p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
